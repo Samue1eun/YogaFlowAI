adee4ab2e5d98b04cb91dab15453e2a5d8f8de34
samue1eun <samuel.eun.park@gmail.com>
2025-12-19 09:34:57 -0600

    This is the full project

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -21,13 +21,13 @@ func ConnectDatabase() {
 	host := os.Getenv("HOST")
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
 	user := os.Getenv("USER")
-	dbname := os.Getenv('DB_NAME')
+	dbname := os.Getenv("DB_NAME")
 	pass := os.Getenv("PASSWORD")
 
 	psqlSetup := fmt.Sprintf("host=%s port=%s user=%s dbname=%s pass=%s sslmode=disable",
 	host, port, user, dbname, pass)
 	db, errSql := sql.Open("postgres", psqlSetup)
-	if errSql := nil {
+	if errSql != nil {
 		fmt.Println("There was an error while connecting to the database", err)
 		panic(err)
 	} else {

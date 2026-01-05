9f976b72a7893d8477e791ba509dbd1d137f4cc7
samue1eun <samuel.eun.park@gmail.com>
2026-01-05 13:49:19 -0600

    Still working on how to make a new YogaFlow. Error with the Pose List

--- backend/handlers/yoga_flow.go
+++ backend/handlers/yoga_flow.go
@@ -1,6 +1,53 @@
 package handlers
 
-import ()
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+
+	"yogaflow.ai/models"
+	"yogaflow.ai/services"
+	"yogaflow.ai/database"
+)
 
 // Need to work on the Scheme before working on yoga_flow handlers
+
+// Get All Yoga Flows
+
+func GetAllYogaFlows (c *gin.Context) {
+	var yogaFlows []models.YogaFlow
+	rows, err := database.Db.Query("SELECT id, type, time_length, number_of_poses, pose_list, average_strength, average_flexibility, average_difficulty")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	
+	for rows.Next() {
+		var yogaFlow models.YogaFlow
+		err := rows.Scan(&yogaFlow.ID, &yogaFlow.Type, &yogaFlow.TimeLength, &yogaFlow.NumberOfPoses, &yogaFlow.PoseList, &yogaFlow.AverageStrength, &yogaFlow.AverageFlexibility, &yogaFlow.AverageDifficulty)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		yogaFlows = append(yogaFlows, yogaFlow)
+	}
+	defer rows.Close()
+	c.IndentedJSON(http.StatusOK, yogaFlows)
+}
+
+
+// Add Yoga Flow
+func CreateYogaFlow (c *gin.Context) {
+	var newYogaFlows models.YogaFlow
+	err := c.ShouldBindJSON(&newYogaFlows)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	yogaFlow, err := services.CreateYogaFlow(newYogaFlows)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, yogaFlow)
+}
--- backend/router/router.go
+++ backend/router/router.go
@@ -13,17 +13,25 @@ func PageRouter() {
 	r := gin.Default()
 	v1 := r.Group("/v1")
 	{
+		// /user ROUTES
 		v1.GET("/users", handlers.GetAllUsers)
 		v1.GET("/users/adminaccess", handlers.GetAllUsersAdmin)
 		// v1.GET("/users/:id", handlers.GetOneUser)
 		v1.DELETE("/users/:id", handlers.DeleteUser)
 		v1.PUT("/users/:id", handlers.UpdateUser)
 		v1.POST("/users", handlers.AddUser)
+
+		// /yoga_poses ROUTES
 		v1.GET("/yoga_poses", handlers.GetAllYogaPoses)
 		v1.GET("/yoga_poses/:id", handlers.GetOneYogaPose)
 		v1.POST("/yoga_poses", handlers.AddYogaPose)
 		v1.POST("/yoga_poses/bulkadd", handlers.AddBulkYogaPoses)
 		v1.DELETE("/yoga_poses/:id", handlers.DeleteYogaPose)
+
+		// /yoga_flow ROUTES
+		v1.GET("/yoga_flow", handlers.GetAllYogaFlows)
+		v1.POST("/yoga_flow", handlers.CreateYogaFlow)
+		
 	}
 
 	r.Run(":8081")

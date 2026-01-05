55afb5bd9149af433966405cde5feea1b9aec6e5
samue1eun <samuel.eun.park@gmail.com>
2026-01-05 16:51:41 -0600

    Added in routes to post and get yoga_flows. Tested endpoints and it worked

--- backend/handlers/yoga_flow.go
+++ backend/handlers/yoga_flow.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,22 +17,37 @@ import (
 
 func GetAllYogaFlows (c *gin.Context) {
 	var yogaFlows []models.YogaFlow
-	rows, err := database.Db.Query("SELECT id, type, time_length, number_of_poses, pose_list, average_strength, average_flexibility, average_difficulty")
+	rows, err := database.Db.Query("SELECT id, type, time_length, number_of_poses, pose_list, average_strength, average_flexibility, average_difficulty FROM yoga_flows")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	defer rows.Close()
 	
 	for rows.Next() {
 		var yogaFlow models.YogaFlow
-		err := rows.Scan(&yogaFlow.ID, &yogaFlow.Type, &yogaFlow.TimeLength, &yogaFlow.NumberOfPoses, &yogaFlow.PoseList, &yogaFlow.AverageStrength, &yogaFlow.AverageFlexibility, &yogaFlow.AverageDifficulty)
+		var poseListJSON []byte
+		err := rows.Scan(
+			&yogaFlow.ID, 
+			&yogaFlow.Type, 
+			&yogaFlow.TimeLength, 
+			&yogaFlow.NumberOfPoses, 
+			&poseListJSON, 
+			&yogaFlow.AverageStrength, 
+			&yogaFlow.AverageFlexibility, 
+			&yogaFlow.AverageDifficulty)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
+		// Unmarshal JSON bytes into []YogaPoses
+        if err := json.Unmarshal(poseListJSON, &yogaFlow.PoseList); err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+            return
+        }
 		yogaFlows = append(yogaFlows, yogaFlow)
 	}
-	defer rows.Close()
+
 	c.IndentedJSON(http.StatusOK, yogaFlows)
 }
 
--- backend/services/yoga_flow.go
+++ backend/services/yoga_flow.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"encoding/json"
+
 	"yogaflow.ai/models"
 	"yogaflow.ai/database"
 )
@@ -8,14 +10,18 @@ import (
 // Need to work on the Schema Table prior to implementing user_flow_services
 
 func CreateYogaFlow (newYogaFlow models.YogaFlow) (models.YogaFlow, error) {
+	poseListJSON, err := json.Marshal(newYogaFlow.PoseList)
+    if err != nil {
+        return newYogaFlow, err
+    }
 	query := `INSERT INTO yoga_flows (id, type, time_length, number_of_poses, pose_list, average_strength, average_flexibility, average_difficulty)
-			VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7)`
-	err := database.Db.QueryRow(
+			VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7) RETURNING id`
+	err = database.Db.QueryRow(
 		query,
 		newYogaFlow.Type,
 		newYogaFlow.TimeLength,
 		newYogaFlow.NumberOfPoses,
-		newYogaFlow.PoseList,
+		poseListJSON,
 		newYogaFlow.AverageStrength,
 		newYogaFlow.AverageFlexibility,
 		newYogaFlow.AverageDifficulty,

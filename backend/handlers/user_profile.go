3994aea414c9322cd84b8fd263eea8978bc03b40
samue1eun <samuel.eun.park@gmail.com>
2026-01-09 21:43:27 -0700

    Working on the handlers for UserProfile

--- backend/handlers/user_profile.go
+++ backend/handlers/user_profile.go
@@ -1,1 +1,42 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"yogaflow.ai/database"
+	"yogaflow.ai/models"
+)
+
+func GetAllUserProfiles (c *gin.Context) {
+	var userProfiles []models.UserProfile
+	// Add in model
+	rows, err := database.Db.Query("SELECT id, user_id, fitness_level, flexibility_level, strength_level, injuries, goals FROM user_profile")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var userProfiles models.YogaFlow
+		var injuriesListJSON []byte
+		var goalsListJSON []byte
+		// Take a look at the model
+		err := rows.Scan(
+			&userProfiles.ID,
+			&userProfiles.UserID,
+			&userProfiles.FitnessLevel,
+			&userProfiles.StrengthLevel,
+			&injuriesListJSON,
+			&goalsListJSON,
+		)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	// Unmarshal JSON bytes into []Injuries and []Goal
+	
+
+	}
+}

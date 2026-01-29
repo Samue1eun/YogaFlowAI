5e278da7c3e6e0a9905aba278884c2231e043205
samue1eun <samuel.eun.park@gmail.com>
2026-01-28 21:55:45 -0900

    Added in my own claims model

---
+++ backend/models/claims.go
@@ -0,0 +1,12 @@
+import (
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	UserID   int    `json:"user_id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Role     string `json:"role"`
+	Tier     string `json:"tier"`
+	jwt.RegisteredClaims
+}
